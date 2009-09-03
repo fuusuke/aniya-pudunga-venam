51397f132ab6d2dd316a51542f2b9e0df2d3910b
sujay.cooldude <sujay.cooldude@4bb7a63e-9890-11de-a05d-3bd73e269465>
2009-09-03 16:06:26 +0000

    Using BrainyQuotes.com

--- trunk/Interview Project/src/com/sakasu/MyLauncherActivity.java
+++ trunk/Interview Project/src/com/sakasu/MyLauncherActivity.java
@@ -27,10 +27,17 @@ import android.text.util.Linkify.TransformFilter;
 import android.util.Xml;
 import android.util.Xml.Encoding;
 import android.view.Gravity;
-import android.view.ViewGroup.LayoutParams;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+/**
+ * for brainy quotes.
+ * 
+ * @author sakasu
+ * 
+ */
 
 public class MyLauncherActivity extends Activity {
 
@@ -39,10 +46,12 @@ public class MyLauncherActivity extends Activity {
 	final static class Quote {
 		String quote;
 		String author;
+
 		public Quote() {
 			quote = new String();
 			author = new String();
 		}
+
 		@Override
 		public String toString() {
 			return author + " - " + quote;
@@ -54,7 +63,7 @@ public class MyLauncherActivity extends Activity {
 	private TextView authorView;
 	private Timer timer;
 	private Handler handler;
-
+	private LinearLayout linearLayout;
 	private QuoteContentHandler quoteContentHandler = new QuoteContentHandler();
 
 	/** Called when the activity is first created. */
@@ -67,12 +76,12 @@ public class MyLauncherActivity extends Activity {
 		quoteView.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
 		quoteView.setClickable(false);
 		authorView = new TextView(this);
-		authorView.setGravity(Gravity.CENTER_VERTICAL|Gravity.RIGHT);
+		authorView.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
 		authorView.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
-		// CharSequence styledResults =
-		// Html.fromHtml("<a href=\"http://www.google.com\">Google</a>");
 
-		// System.out.println(styledResults);
+		linearLayout = new LinearLayout(this);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+
 		new Thread() {
 			public void run() {
 				DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -86,30 +95,23 @@ public class MyLauncherActivity extends Activity {
 						Xml.parse(entity.getContent(), Encoding.UTF_8,
 								quoteContentHandler);
 					} catch (IllegalStateException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					} catch (SAXException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					quotes.addAll(quoteContentHandler.getReceivedQuotes());
 				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			};
 		}.start();
 
-		// this.setListAdapter(new ArrayAdapter<String>(this,
-		// R.layout.simple_list_item_1, new String[] { "9620927739",
-		// "9739479914", "9611827822" }));
+		linearLayout.addView(quoteView);
+		linearLayout.addView(authorView);
+		setContentView(linearLayout);
 
-		setContentView(quoteView);
-		addContentView(authorView, new LayoutParams(LayoutParams.FILL_PARENT,
-				LayoutParams.FILL_PARENT));
 		TimerTask tt = new TimerTask() {
 			Random randomizer = new Random();
 
@@ -127,9 +129,7 @@ public class MyLauncherActivity extends Activity {
 		timer.scheduleAtFixedRate(tt, 0L, 5000);
 	}
 
-
 	private void initialize() {
-		// TODO Auto-generated method stub
 		handler = new Handler() {
 			Animation fadeOutAnimation = AnimationUtils.loadAnimation(
 					MyLauncherActivity.this, R.anim.fade_out);
@@ -138,21 +138,22 @@ public class MyLauncherActivity extends Activity {
 			MatchFilter matchFilter = new MatchFilter() {
 
 				@Override
-				public boolean acceptMatch(CharSequence s, int start,
-						int end) {
+				public boolean acceptMatch(CharSequence s, int start, int end) {
 					return true;
 				}
 			};
-			TransformFilter transformFilter =  new TransformFilter() {
+			TransformFilter transformFilter = new TransformFilter() {
 
 				@Override
 				public String transformUrl(Matcher match, String url) {
 					url = url.toLowerCase();
-					if(url.length()>0)
-						return url.charAt(0)+"/"+url.replace(' ', '_') + ".html";
+					if (url.length() > 0)
+						return url.charAt(0) + "/" + url.replace(' ', '_')
+								+ ".html";
 					return url;
 				}
 			};
+
 			@Override
 			public void handleMessage(Message msg) {
 				switch (msg.what) {
@@ -163,14 +164,15 @@ public class MyLauncherActivity extends Activity {
 					System.out.println("***********");
 					System.out.println(quotes);
 					System.out.println(msg.arg1);
-					System.out.println(quote.author+" - "+quote.quote);
+					System.out.println(quote.author + " - " + quote.quote);
 					System.out.println("***********");
 					quoteView.setText(quote.quote);
-					authorView.setText(quote.author);
+					authorView.setText("\n\n\n  " + quote.author);
 					quoteView.startAnimation(fadeInAnimation);
 					authorView.startAnimation(fadeInAnimation);
-					Linkify.addLinks(authorView, Pattern.compile(".*"), "http://www.brainyquote.com/quotes/authors/",
-							matchFilter,transformFilter);
+					Linkify.addLinks(authorView, Pattern.compile(".*"),
+							"http://www.brainyquote.com/quotes/authors/",
+							matchFilter, transformFilter);
 
 				}
 			}
